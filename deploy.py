05c7456c747b2666b7987aeb4d6b839d137bcfaa
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-12-25 10:25:41 +0700

    init

---
+++ deploy.py
@@ -0,0 +1,70 @@
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+from web3._utils.filters import TransactionFilter
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with open("SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# Compile Solidity
+install_solc("0.8.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.8.0",
+)
+
+# Output the compiled code into a JSON file
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# Get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# Get ABI
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# Connecting to Ganache (local blockchain)
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0xF9D91d066F4440ed25fe6511e2f51867b3a05184"
+private_key = os.getenv("PRIVATE_KEY")
+
+# Create contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get latest transaction for nonce value
+nonce = w3.eth.getTransactionCount(my_address)
+
+# ----------------------
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+# ----------------------
+# build transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# sign transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# send this signed transaction
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# just for awaiting block confirmation, a good practice before executing next code
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
